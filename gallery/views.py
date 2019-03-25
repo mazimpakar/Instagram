e2bfbdc228accdcd9a22a5da546683b0473b077f
mazimpakar <rosemazimpaka2@gmail.com>
2019-03-25 17:39:38 +0200

    add render of our request

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,81 @@
-from django.shortcuts import render
-from django.http  import HttpResponse
-
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+import datetime as dt
+from .models import Image,GalleryLetterRecipients
+from .forms import NewImageForm,GalleryLetterForm
+from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def welcome(request):
-    return render(request, 'welcome.html')
+    return render(request, 'today-gallery.html', {"date": date,})
+
+def todays_gallery(request):
+    date = dt.date.today()
+    # gallery = Image.todays_gallery()
+    if request.method == 'POST':
+        form = GalleryLetterForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+
+            recipient = galleryLetterRecipients(name = name,email =email)
+            recipient.save()
+            send_welcome_email(name,email)
+            HttpResponseRedirect('todays_gallery')
+    else:
+        form = GalleryLetterForm()
+    return render(request,'todays_gallery.html',{"date": date,"letterForm":form})
+# def convert_dates(dates):
+# View Function to present news from past days
+
+def past_days_gallery(request, past_date):
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(gallery_today)
+
+    gallery = Image.days_gallery(date)
+    return render(request, 'past_gallery.html',{"date": date,"gallery":gallery})
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_caption(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+
+@login_required(login_url='/accounts/login/')
+def image(request, images_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"image.html", {"image":images})
+@login_required(login_url='/accounts/login/')
+def new_image(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            images.user = current_user
+            image.save()
+        return redirect('todaysGallery')
+
+    else:
+        form = NewImageForm()
+    return render(request, 'new_image.html', {"form": form})    
+
