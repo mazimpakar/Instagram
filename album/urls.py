a88d460f11caa8286c99c07fb03763465708a5bf
mazimpakar <rosemazimpaka2@gmail.com>
2019-03-26 19:20:56 +0200

    add project name

--- album/urls.py
+++ album/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 
 from django.conf.urls import url,include
 from django.contrib import admin
-
+from django.contrib.auth import views 
 
 urlpatterns = [
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
     url(r'^admin/', admin.site.urls),
-    url(r'',include('gallery.urls'))
+    url(r'',include('gallery.urls')),
+    url(r'^tinymce/', include('tinymce.urls')) 
 ]
