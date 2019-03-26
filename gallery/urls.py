f928b94e935eb0b8e6795ed2447d4103d16b1c5c
mazimpakar <rosemazimpaka2@gmail.com>
2019-03-26 18:55:47 +0200

    add test of app

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url(r'^image/(\d+)',views.image,name ='image'),
+    url(r'^$', views.gallery_today, name = 'todaysGallery'),
+    # url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_gallery,name = 'pastGallery'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^new/image$', views.new_image, name='new-image')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
